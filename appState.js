1b5d98f34cc27a6e0b4c5f3999877000a666b099
jason-c-child <child.jason@icloud.com>
2017-01-18 11:24:18 -0800

    init

---
+++ appState.js
@@ -0,0 +1,26 @@
+import EventEmitter from 'events'
+
+const appState = seedState => {
+  const emitter = new EventEmitter()
+  let state = seedState || {}
+ 
+  emitter.on('set', function(payload) {
+      state = payload
+      this.callback(state)
+  })
+  
+  emitter.on('reset', function() {
+      seedState && (state = seedState)
+      this.callback(state)
+  })
+  
+  emitter.on('update', function(payload) {
+      state = {...state, ...payload}
+      this.callback(state)
+  })
+  
+  return {
+    emitter,
+    state
+  }
+}
